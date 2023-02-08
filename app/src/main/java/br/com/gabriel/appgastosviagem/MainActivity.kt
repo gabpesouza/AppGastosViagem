20d78638edd4b151312c27fef840003c3537c149
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2023-02-08 08:29:55 -0300

    ClickListener e ToastMsg implementados.

--- app/src/main/java/br/com/gabriel/appgastosviagem/MainActivity.kt
+++ app/src/main/java/br/com/gabriel/appgastosviagem/MainActivity.kt
@@ -2,9 +2,11 @@ package br.com.gabriel.appgastosviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import br.com.gabriel.appgastosviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding;
 
@@ -13,9 +15,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater);
         setContentView(binding.root);
 
+        binding.buttonCalculate.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+       if(view.id == R.id.button_calculate){
+           calculate();
+       }
     }
 
     private fun calculate(){
-
+        Toast.makeText(this, "Botao clicado!",Toast.LENGTH_SHORT).show();
     }
+
+
+
 }
